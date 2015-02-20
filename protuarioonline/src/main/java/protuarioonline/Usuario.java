24197f6f6adc4423e4f43bcb892268901a1b6ed9
ruby <wandergontijo@hotmail.com>
2015-02-20 19:16:16 -0200

    seundo commmit

--- protuarioonline/src/main/java/protuarioonline/Usuario.java
+++ protuarioonline/src/main/java/protuarioonline/Usuario.java
@@ -1,5 +1,15 @@
 package protuarioonline;
 
 public class Usuario {
+	int Codigo;
+	String Nome;
+	String Senha;
+	
+	boolean Login(String Nome, String Senha){
+		
+	 return (Nome =="admin") && (Senha=="123456");
+		
+	}
+	
 
 }
